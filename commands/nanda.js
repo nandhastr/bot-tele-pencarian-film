c0b5fefcbe99c464e0b9a56bdca54b23858d178b
nandhastr <nandhastr2@gmail.com>
2025-08-29 15:40:10 +0700

    fix image path

--- commands/nanda.js
+++ commands/nanda.js
@@ -1,21 +1,24 @@
-// commands/nanda.js
-export default async (bot, chatId) => {
-    const photoUrl = "https://botpencarianfilm.vercel.app/image/nanda.jpg";
-
-    try {
-        const response = await fetch(photoUrl);
-        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-
-        const arrayBuffer = await response.arrayBuffer();
-        const buffer = Buffer.from(arrayBuffer);
-
-        await bot.sendPhoto(chatId, buffer, {
-            caption: "Nanda teh orang bogor, nu resep ngoding, sareng ngadamel aplikasi web. Ayeuna nuju diajar lewih jero ngeunaan pemrograman. Nu bade kenal langkung jero tiasa di tingali di (https://nandaportfolio.my.id). Hatur nuhun.\n\nKlik tombol ieu /start kanggo balik deui ka menu utama.",
-            parse_mode: "Markdown",
-        });
-        
-        console.log("Foto berhasil dikirim ke chat:", chatId);
-    } catch (err) {
-        console.error("Gagal kirim foto:", err.message);
-    }
+export default async(bot, chatId) => {
+    bot.sendMessage(chatId, "Nanda teh orang bogor, nu resep ngoding, sareng ngadamel aplikasi web. Ayeuna nuju diajar lewih jero ngeunaan pemrograman. Nu bade kenal langkung jero tiasa di tingali di (https://nandaportfolio.my.id). Hatur nuhun.\n\nKlik tombol ieu /start kanggo balik deui ka menu utama.", { parse_mode: "Markdown" });
 };
+
+// export default async (bot, chatId) => {
+//     const photoUrl = "https://botpencarianfilm.vercel.app/image/nanda.jpg";
+
+//     try {
+//         const response = await fetch(photoUrl);
+//         if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+
+//         const arrayBuffer = await response.arrayBuffer();
+//         const buffer = Buffer.from(arrayBuffer);
+
+//         await bot.sendPhoto(chatId, buffer, {
+//             caption: "Nanda teh orang bogor, nu resep ngoding, sareng ngadamel aplikasi web. Ayeuna nuju diajar lewih jero ngeunaan pemrograman. Nu bade kenal langkung jero tiasa di tingali di (https://nandaportfolio.my.id). Hatur nuhun.\n\nKlik tombol ieu /start kanggo balik deui ka menu utama.",
+//             parse_mode: "Markdown",
+//         });
+        
+//         console.log("Foto berhasil dikirim ke chat:", chatId);
+//     } catch (err) {
+//         console.error("Gagal kirim foto:", err.message);
+//     }
+// };

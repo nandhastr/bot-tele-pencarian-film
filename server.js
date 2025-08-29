b229815b7978e4303145188982df8f1ae7c68503
nandhastr <nandhastr2@gmail.com>
2025-08-29 14:55:21 +0700

    fix image to view

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+// Atur folder public untuk static file
+const __dirname = path.resolve();
+app.use("/image", express.static(path.join(__dirname, "public/image")));
+
 
 app.use("/api/telegram", telegramRoutes)
 

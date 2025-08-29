a8fefa1dd7180f4f5c0b9940f9ae6dc5ed4e4d30
nandhastr <nandhastr2@gmail.com>
2025-08-29 11:19:21 +0700

    fix telegramroute

--- routes/TelegramRoutes.js
+++ routes/TelegramRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import TelegramBot from "node-telegram-bot-api";
-import { telegramController } from "../Controller/telegramController.js";
+import { telegramController } from "../Controller/TelegramController.js";
 
 const router = express.Router();
 
@@ -21,6 +21,10 @@ if (process.env.NODE_ENV === "production") {
     bot = new TelegramBot(token, { polling: true }); // lokal polling
     console.log("Bot berjalan dalam mode (local dev)");
 }
+
+//controlle
+telegramController(bot);
+
 
 router.get("/", (req, res) => {
     res.send("Telegram Bot berhasil dijalankan.");
